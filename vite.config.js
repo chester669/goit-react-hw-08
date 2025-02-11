a866db35b9212e785ee7a5d3b0d40149a810afa4
chester669 <viktorostash44@gmail.com>
2025-02-11 11:55:00 +0200

    check

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    sourcemap: true,
   },
-  server: {
-    port: 3000,
-  },
-  base: "./",
 });
